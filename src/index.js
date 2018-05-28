626e67547b95038b2a670914e10465aede52b014
Mac Read <james.macgregor.read@gmail.com>
2018-05-28 15:00:32 -0600

    Step 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Hashrouter } from 'react-reouter-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
+ReactDOM.render(
+    <Provider store = { store }>
+        <Hashrouter>
+            <App />
+        </Hashrouter>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
 
